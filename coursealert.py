c4c473c3fa2fc6b3f2f7253d3a0ae24b7bc7c3b3
Chan Woo Park <14cpark@gmail.com>
2018-12-09 12:58:05 -0800

    make request & grab texts, handle error responses

--- coursealert.py
+++ coursealert.py
@@ -1,10 +1,28 @@
 from flask import *
+import requests
 
 app = Flask(__name__)
+URL = 'https://www.reg.uci.edu/perl/WebSoc'
+
+DATA = 'Submit=Display+Web+Results&YearTerm=2019-03&ShowComments=on&ShowFinals=on&Breadth=ANY&Dept=+ALL&CourseNum=&Division=ANY&CourseCodes=34130&InstrName=&CourseTitle=&ClassType=ALL&Units=&Days=&StartTime=&EndTime=&MaxCap=&FullCourses=ANY&FontSize=100&CancelledCourses=Exclude&Bldg=&Room='
 
 @app.route("/")
-def index():  
-    return "Hello World!"
+def index():
+	response = requests.post(url = URL, data = DATA)
+	return processResponseString(response)
+	
 
+
+def processResponseString(response):
+	rtext = response.text
+	if ("No courses matched your search criteria for this term." in rtext) or ("Error:" in rtext):
+		return "ERROR"
+	
+	index = rtext.find('</font></td></tr>')
+	indexstart = index - 6
+	return rtext[indexstart : indexstart + 6]
+	#return rtext
+	
+	
 if __name__ == "__main__":  
     app.run(port=8080)
