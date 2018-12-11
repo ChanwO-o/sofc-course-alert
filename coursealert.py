65511c89c2f9a6981e5323566cc6c77fa331bc75
Chan Woo Park <14cpark@gmail.com>
2018-12-10 16:48:43 -0800

    handle responses by digging through html tags using BeautifulSoup

--- coursealert.py
+++ coursealert.py
@@ -1,27 +1,33 @@
 from flask import *
 import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 URL = 'https://www.reg.uci.edu/perl/WebSoc'
 
-DATA = 'Submit=Display+Web+Results&YearTerm=2019-03&ShowComments=on&ShowFinals=on&Breadth=ANY&Dept=+ALL&CourseNum=&Division=ANY&CourseCodes=34130&InstrName=&CourseTitle=&ClassType=ALL&Units=&Days=&StartTime=&EndTime=&MaxCap=&FullCourses=ANY&FontSize=100&CancelledCourses=Exclude&Bldg=&Room='
+DATA = 'Submit=Display+Web+Results&YearTerm=2019-03&ShowComments=on&ShowFinals=on&Breadth=ANY&Dept=+ALL&CourseNum=&Division=ANY&CourseCodes=34140&InstrName=&CourseTitle=&ClassType=ALL&Units=&Days=&StartTime=&EndTime=&MaxCap=&FullCourses=ANY&FontSize=100&CancelledCourses=Exclude&Bldg=&Room='
 
 @app.route("/")
 def index():
 	response = requests.post(url = URL, data = DATA)
 	return processResponseString(response)
-	
+
 
 
 def processResponseString(response):
 	rtext = response.text
-	if ("No courses matched your search criteria for this term." in rtext) or ("Error:" in rtext):
-		return "ERROR"
+
 	
-	index = rtext.find('</font></td></tr>')
-	indexstart = index - 6
-	return rtext[indexstart : indexstart + 6]
-	#return rtext
+	soup = BeautifulSoup(rtext, 'html.parser')
+	allfonttags = soup.findAll('font') # returns list of html elements
+	
+	
+	if len(allfonttags) == 0:
+		return 'ERROR no courses found'
+	elif len(allfonttags) == 2: # will return list of length 2 only if course is FULL
+		return str(allfonttags[0]) + '	' + 'FULL'
+	elif len(allfonttags) == 3: # course is OPEN or Waitl
+		return str(allfonttags[0]) + '	' + str(allfonttags[1])
 	
 	
 if __name__ == "__main__":  
