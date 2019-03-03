a93fde671ad647e12575e497eb860a20375c5320
Chan Woo <14cpark@gmail.com>
2019-03-03 08:27:24 -0800

    extract course name using getCourseName()

--- coursealert.py
+++ coursealert.py
@@ -6,31 +6,35 @@ import send_sms
 app = Flask(__name__)
 URL = 'https://www.reg.uci.edu/perl/WebSoc'
 
-DATA = 'Submit=Display+Web+Results&YearTerm=2019-03&ShowComments=on&ShowFinals=on&Breadth=ANY&Dept=+ALL&CourseNum=&Division=ANY&CourseCodes=34140&InstrName=&CourseTitle=&ClassType=ALL&Units=&Days=&StartTime=&EndTime=&MaxCap=&FullCourses=ANY&FontSize=100&CancelledCourses=Exclude&Bldg=&Room='
+DATA = 'Submit=Display+Web+Results&YearTerm=2019-14&ShowComments=on&ShowFinals=on&Breadth=ANY&Dept=+ALL&CourseNum=&Division=ANY&CourseCodes=34160&InstrName=&CourseTitle=&ClassType=ALL&Units=&Days=&StartTime=&EndTime=&MaxCap=&FullCourses=ANY&FontSize=100&CancelledCourses=Exclude&Bldg=&Room='
 
 @app.route("/")
 def index():
 	response = requests.post(url = URL, data = DATA)
 	coursetuple = processResponseString(response)
-	send_sms.sendMessage(coursetuple)
-	return coursetuple
+	# send_sms.sendMessage(coursetuple)
+	return 'asdf'
 
 
 def processResponseString(response):
+	""" Return a tuple with information on course. In the form of (name, code, type, enrcount, waitlist, status)  """
 	rtext = response.text
-
-	
 	soup = BeautifulSoup(rtext, 'html.parser')
-	allfonttags = soup.findAll('font') # returns list of html elements
 	
+	name = getCourseName(soup)
+	print(name)
 	
-	if len(allfonttags) == 0:
-		return tuple('ERROR no courses found')
-	elif len(allfonttags) == 2: # will return list of length 2 only if course is FULL
-		return tuple((allfonttags[0].getText(), 'FULL'))
-	elif len(allfonttags) == 3: # course is OPEN or Waitl
-		return tuple((allfonttags[0].getText(), allfonttags[1].getText()))
+	# allfonttags = soup.findAll('font') # returns list of html elements
+	# if len(allfonttags) == 0:
+		# return tuple('ERROR no courses found')
+	# elif len(allfonttags) == 2: # will return list of length 2 only if course is FULL
+		# return tuple((allfonttags[0].getText(), 'FULL'))
+	# elif len(allfonttags) == 3: # course is OPEN or Waitl
+		# return tuple((allfonttags[0].getText(), allfonttags[1].getText()))
 	
+def getCourseName(soup):
+	""" Extract name from response """
+	return soup.find('tr', {'bgcolor' : '#fff0ff'}).find('b').text # tr tag with bgcolor -> get bold tag -> text
 	
 if __name__ == "__main__":  
     app.run(port=8080)
