f5a25c8e3d75c78266817376513b7af302df085e
Chan Woo <14cpark@gmail.com>
2019-03-05 15:53:38 -0800

    send requests with intervals store time as constants

--- coursealert.py
+++ coursealert.py
@@ -5,6 +5,9 @@ import time
 
 app = Flask(__name__)
 
+TIME_BETWEEN_REQUESTS = 60 # 1 min
+TIME_BETWEEN_CHECKS = 3600 # 1 hr
+COURSECODES = [34040, 34090, 34150, 34165, 34166, 34167]
 
 @app.route("/")
 def index():
@@ -12,15 +15,18 @@ def index():
 	t.daemon = True
 	t.start()
 
-	
 	return 'asdf' #coursetupleToString(coursetuple)
 
 def startWatchingCourse():
 	while True:
-		coursetuple = websoc.requestCourse(34150)
-		print(coursetuple)
-		send_sms.sendMessage(coursetuple)
-		time.sleep(60)
+		for code in COURSECODES:
+			coursetuple = websoc.requestCourse(code)
+			print(coursetupleToString(coursetuple))
+			# compare contents of coursetuple with database to see changes
+			# if status change, send sms
+			send_sms.sendMessage(coursetuple)
+			time.sleep(TIME_BETWEEN_REQUESTS) # distance each course request by 1 min
+		time.sleep(TIME_BETWEEN_CHECKS) # perform total check every 1 hr
 	
 def coursetupleToString(coursetuple):
 	result = ''
