0e0414718e4b9cd64c7bc5ba6c305b0ae5fb1ba9
Chan Woo <14cpark@gmail.com>
2019-03-05 11:55:31 -0800

    basic setup of threading

--- coursealert.py
+++ coursealert.py
@@ -1,16 +1,30 @@
 from flask import *
 import websoc, send_sms
+import threading
+import time
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
+	t = threading.Thread(target=startWatchingCourse)
+	t.daemon = True
+	t.start()
+	
+	'''
 	coursetuple = websoc.requestCourse(34150)
 	print(coursetuple)
 	# send_sms.sendMessage(coursetuple)
-	return coursetupleToString(coursetuple)
+	'''
+	
+	return 'asdf' #coursetupleToString(coursetuple)
 
+def startWatchingCourse():
+	while True:
+		time.sleep(5)
+		print('sleep')
+	
 def coursetupleToString(coursetuple):
 	result = ''
 	for i in coursetuple:
