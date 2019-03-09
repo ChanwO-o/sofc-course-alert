2862e281653904500ed4ea98ab9f826a03e4aa2e
Chan Woo <14cpark@gmail.com>
2019-03-09 10:56:16 -0800

    send one request for all courses: fixed entire code to support single request structure

--- coursealert.py
+++ coursealert.py
@@ -5,28 +5,25 @@ import time
 
 app = Flask(__name__)
 
-TIME_BETWEEN_REQUESTS = 60 # 1 min
-TIME_BETWEEN_CHECKS = 3600 # 1 hr
+TIME_BETWEEN_REQUESTS = 3600 # 1 hr
 COURSECODES = [34040, 34090, 34150, 34165, 34166, 34167]
 
 @app.route("/")
 def index():
 	t = threading.Thread(target=startWatchingCourse)
-	t.daemon = True
+	t.daemon = True # kill thread when main thread dies
 	t.start()
 
 	return 'asdf' #coursetupleToString(coursetuple)
 
 def startWatchingCourse():
 	while True:
-		for code in COURSECODES:
-			coursetuple = websoc.requestCourse(code)
-			print(coursetupleToString(coursetuple))
-			# compare contents of coursetuple with database to see changes
-			# if status change, send sms
-			send_sms.sendMessage(coursetuple)
-			time.sleep(TIME_BETWEEN_REQUESTS) # distance each course request by 1 min
-		time.sleep(TIME_BETWEEN_CHECKS) # perform total check every 1 hr
+		coursetuple = websoc.requestCourses(COURSECODES)
+		print(coursetupleToString(coursetuple))
+		# compare contents of coursetuple with database to see changes
+		# if status change, send sms
+		# send_sms.sendMessage(coursetuple)
+		time.sleep(TIME_BETWEEN_REQUESTS) # perform total check every 1 hr
 	
 def coursetupleToString(coursetuple):
 	result = ''
--- websoc.py
+++ websoc.py
@@ -3,18 +3,32 @@ from bs4 import BeautifulSoup
 
 URL = 'https://www.reg.uci.edu/perl/WebSoc'
 
-def constructRequestString(code):
+def constructRequestString(codes : [int]):
 	""" Construct the string to pass to websoc request """
-	return'Submit=Display+Web+Results&YearTerm=2019-14&ShowComments=on&ShowFinals=on&Breadth=ANY&Dept=+ALL&CourseNum=&Division=ANY&CourseCodes=' + str(code) + '&InstrName=&CourseTitle=&ClassType=ALL&Units=&Days=&StartTime=&EndTime=&MaxCap=&FullCourses=ANY&FontSize=100&CancelledCourses=Exclude&Bldg=&Room='
+	return'Submit=Display+Web+Results&YearTerm=2019-14&ShowComments=on&ShowFinals=on&Breadth=ANY&Dept=+ALL&CourseNum=&Division=ANY&CourseCodes=' + constructCourseCodeString(codes) + '&InstrName=&CourseTitle=&ClassType=ALL&Units=&Days=&StartTime=&EndTime=&MaxCap=&FullCourses=ANY&FontSize=100&CancelledCourses=Exclude&Bldg=&Room='
 
-def requestCourse(code):
-	""" Make request for information on course by provided course code """
-	response = requests.post(url = URL, data = constructRequestString(code))
-	coursetuple = processResponse(response, code)
+	'''	Submit=Display+Web+Results&YearTerm=2019-14&ShowComments=on&ShowFinals=on&Breadth=ANY&Dept=+ALL&CourseNum=&Division=ANY&CourseCodes=34040+34090+34150&InstrName=&CourseTitle=&ClassType=ALL&Units=&Days=&StartTime=&EndTime=&MaxCap=&FullCourses=ANY&FontSize=100&CancelledCourses=Exclude&Bldg=&Room=
+	'''
+	
+def constructCourseCodeString(codes : [int]):
+	""" Construct course code portion of request string in the form of 34040+34090+34150 """
+	if len(codes) == 0:
+		return None
+	if len(codes) == 1:
+		return str(codes[0])
+	result = str(codes[0])
+	for i in range(1, len(codes)):
+		result += '+' + str(codes[i])
+	return result
+	
+def requestCourses(codes):
+	""" Make request for information on course by provided course codes """
+	response = requests.post(url = URL, data = constructRequestString(codes))
+	coursetuple = processResponse(response, codes)
 	return coursetuple
 
-def processResponse(response, code):
-	""" Return a tuple with information on course. In the form of (name, code, type, max, enroll, waitlist, status)  """
+def processResponse(response, codes):
+	""" Return a list of tuples with information on courses. In the form of (name, code, type, max, enroll, waitlist, status)  """
 	rtext = response.text
 	soup = BeautifulSoup(rtext, 'html.parser')
 	
@@ -24,17 +38,28 @@ def processResponse(response, code):
 		print('ERROR no courses found')
 		return tuple('ERROR no courses found')
 	
-	name = getCourseName(soup)
-	type = getCourseType(soup)
-	max = getCourseMax(soup)
-	enroll = getCourseEnroll(soup)
-	waitlist = getCourseWaitlist(soup)
-	status = getCourseStatus(soup)
-	return(name, code, type, max, enroll, waitlist, status)
+	courseshtmlblocks = splitHtmlByCourse(soup) # list of html blocks per course
+	
+	result = []
+	for htmlblock in courseshtmlblocks:
+		soup = BeautifulSoup(htmlblock, 'html.parser')
+		cname = getCourseName(soup)
+		ctype = getCourseType(soup)
+		cmax = getCourseMax(soup)
+		cenroll = getCourseEnroll(soup)
+		cwaitlist = getCourseWaitlist(soup)
+		cstatus = getCourseStatus(soup)
+		print((cname, ctype, cmax, cenroll, cwaitlist, cstatus))
+	return result
+	
+def splitHtmlByCourse(soup):
+	""" Split html into blocks by course & returned as a list """
+	courses = soup.prettify().split('<tr bgcolor="#fff0ff" valign="top">') # start of every <tr> tag that declares course name
+	return courses[1:len(courses)]
 	
 def getCourseName(soup):
 	""" Extract name from response """
-	return soup.find('tr', {'bgcolor' : '#fff0ff'}).find('b').text # tr tag with bgcolor -> get bold tag -> text
+	return soup.find('b').text # tr tag with bgcolor -> get bold tag -> text
 	
 def getCourseType(soup):
 	""" Extract course type from response (lec, dis, lab, etc) """
