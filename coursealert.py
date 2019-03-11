149e9e35d1a253db72fb878642bbff4695d36746
Chan Woo <14cpark@gmail.com>
2019-03-10 17:31:01 -0700

    add extra option to get simplified sms format separate sms build to constructBody() & constructVerboseBody()

--- coursealert.py
+++ coursealert.py
@@ -5,7 +5,7 @@ import time
 
 app = Flask(__name__)
 
-TIME_BETWEEN_REQUESTS = 3600 # 1 hr
+TIME_BETWEEN_REQUESTS = 300 #3600 # 1 hr
 COURSECODES = [34040, 34090, 34150, 34165, 34166, 34167]#[383, 34090]#
 
 @app.route("/")
@@ -22,8 +22,7 @@ def startWatchingCourse():
 		# print(coursesdataToString(coursesdatalist))
 		# compare contents of coursesdatalist with database to see changes
 		# if status change, send sms
-		send_sms.sendMessage(coursesdatalist)
-		break
+		send_sms.sendMessage(coursesdatalist, verbose=False)
 		time.sleep(TIME_BETWEEN_REQUESTS) # perform total check every 1 hr
 	
 def coursesdataToString(coursesdatalist):
--- send_sms.py
+++ send_sms.py
@@ -6,15 +6,28 @@ auth_token = "ca52852b96d616c75e529ed4ea06045c"
 
 client = Client(account_sid, auth_token)
 
-def sendMessage(coursesdatalist):
+def sendMessage(coursesdatalist, verbose=True):
 	""" Format text message content with information from coursesdatalist [nameshort, namelong, code, type, max, enroll, waitlist, status]  """
+	smsbody = None
+	if verbose:
+		smsbody = "SofC\n" + constructVerboseBody(coursesdatalist)
+	else:
+		smsbody = "SofC\n" + constructBody(coursesdatalist)
 	client.messages.create(
 		to = "+13106668374",
 		from_= "+12132796150",
-		body = "SofC\n" + constructBody(coursesdatalist)
+		body = smsbody
 	)
 	
 def constructBody(coursesdatalist):
+	result = ''
+	for classtuple in coursesdatalist:
+		classinfo = \
+		classtuple[0] + " " + str(classtuple[2]) + classtuple[7] + "\n."
+		result += classinfo
+	return result
+
+def constructVerboseBody(coursesdatalist):
 	result = ''
 	for classtuple in coursesdatalist:
 		classinfo = \
