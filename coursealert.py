792270f7346783e52f59e1f1b2717986e4325d00
Chan Woo <14cpark@gmail.com>
2019-03-11 17:01:26 -0700

    toggle 'text open only' mode

--- coursealert.py
+++ coursealert.py
@@ -6,7 +6,8 @@ import time
 app = Flask(__name__)
 
 TIME_BETWEEN_REQUESTS = 300 #3600 # 1 hr
-COURSECODES = [36452,34160,36451,36452,36453,36454,36455,36456] # 34165, 34166, 34167, 34168, 34169, 34170, 34171, 34172] #34150, 36451, 36452, 36453, 36454, 36455, 36456] #[34040, 34090, 34150, 34165, 34166, 34167]#[383, 34090]#
+COURSECODES = [36451,36452,36453,36454,36455,36456,34160,34166,34167,34168,34169,34170,34171,34172,34130]
+#[36451,36452,36453,36454,36455,36456,34160,34166,34167,34168,34169,34170,34171,34172,34130]
 #36452 34160 36451 36452 36453 36454 36455 36456
 
 @app.route("/")
@@ -23,15 +24,17 @@ def startWatchingCourse():
 		print(coursesdataToString(coursesdatalist))
 		# ULTIMATE GOAL: compare contents of coursesdatalist with database to see changes
 		# if status change, send sms
-		
-		# for now, just text OPEN classes only
+		# for now, just text all OPEN classes
 		openclasses = []
 		for classtuple in coursesdatalist:
 			if classtuple[-1] == 'OPEN':
 				openclasses.append(classtuple)
 
-		send_sms.sendMessage(coursesdatalist, verbose=False) # text entire class list
-		send_sms.sendMessage(openclasses, verbose=False) # text open classes only
+		# send_sms.sendMessage(coursesdatalist, verbose=False) # option 1: text entire class list
+		
+		if len(openclasses) > 0:
+			print(openclasses)
+			send_sms.sendMessage(openclasses, verbose=False) # option 2: text open classes only (if there are any)
 		
 		time.sleep(TIME_BETWEEN_REQUESTS) # perform total check every 5 minutes
 	
